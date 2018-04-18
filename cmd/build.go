ec97fff137d27a8c1e844d3dbc0025b5915dbb6a
Fabian Holler <fabian.holler@simplesurance.de>
2018-04-18 17:29:43 +0200

    build: fix function name and use sblog.Fatal

--- cmd/build.go
+++ cmd/build.go
@@ -1,9 +1,7 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
+	"github.com/simplesurance/sisubuild/sblog"
 	"github.com/spf13/cobra"
 )
 
@@ -14,11 +12,10 @@ func init() {
 var buildCmd = &cobra.Command{
 	Use:   "build <PATH>|<APP-NAME>|all",
 	Short: "builds an application",
-	Run:   printVersion,
+	Run:   build,
 	Args:  cobra.ExactArgs(1),
 }
 
-func printVersion(cmd *cobra.Command, args []string) {
-	fmt.Println("not implemented")
-	os.Exit(1)
+func build(cmd *cobra.Command, args []string) {
+	sblog.Fatal("not implemented")
 }
