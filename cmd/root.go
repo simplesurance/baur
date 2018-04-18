e9803ea96fa4cd84d2d8a5b8f15ae1dddf98ee83
Fabian Holler <fabian.holler@simplesurance.de>
2018-04-18 17:26:27 +0200

    sb: add --verbose/-v parameter
    Add a --verbose and -v parameter to activate verbose logging

--- cmd/root.go
+++ cmd/root.go
@@ -4,17 +4,28 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/simplesurance/sisubuild/sblog"
 	"github.com/simplesurance/sisubuild/version"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-	Use:     "sb",
-	Short:   "sisubuild manages builds and artifacts in mono repositories.",
-	Version: version.FullVerNr(),
+	Use:              "sb",
+	Short:            "sisubuild manages builds and artifacts in mono repositories.",
+	Version:          version.FullVerNr(),
+	PersistentPreRun: initSb,
+}
+
+var VerboseFlag bool
+
+func initSb(_ *cobra.Command, _ []string) {
+	sblog.EnableDebug(VerboseFlag)
+
 }
 
 func Execute() {
+	rootCmd.PersistentFlags().BoolVarP(&VerboseFlag, "verbose", "v", false, "verbose output")
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
