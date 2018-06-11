3bdd35cf837eaf755f5d5ec52859edfd850ae979
Fabian Holler <fabian.holler@simplesurance.de>
2018-06-11 16:57:57 +0200

    log: synchronize output
    synchronize the output to stdout and stderr, to prevent that messages
    overlap each other

--- log/log.go
+++ log/log.go
@@ -3,7 +3,10 @@ package log
 import (
 	"fmt"
 	"os"
+	"sync"
 )
+
+var lock = sync.Mutex{}
 
 // DebugEnabled set to true to print debug message otherwise they are suppressed
 var DebugEnabled bool
@@ -12,6 +15,9 @@ const actionPrefix = "* "
 
 // Actionln prints something with the ActionPrefix preprended
 func Actionln(v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	v = append([]interface{}{actionPrefix}, v)
 	fmt.Println(v...)
 
@@ -19,12 +25,18 @@ func Actionln(v ...interface{}) {
 
 // Actionf prints something with the ActionPrefix preprended
 func Actionf(format string, v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Printf(actionPrefix+format, v...)
 }
 
 // Debugln logs a debug message to stdout.
 // It's only shown if debugging is enabled.
 func Debugln(v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	if !DebugEnabled {
 		return
 	}
@@ -35,6 +47,9 @@ func Debugln(v ...interface{}) {
 // Debugf logs a debug message to stdout.
 // It's only shown if debugging is enabled.
 func Debugf(format string, v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	if !DebugEnabled {
 		return
 	}
@@ -44,32 +59,50 @@ func Debugf(format string, v ...interface{}) {
 
 // Fatalln logs a message to stderr and terminates the application with an error
 func Fatalln(v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Fprintln(os.Stderr, v...)
 	os.Exit(1)
 }
 
 // Fatalf logs a message to stderr and terminates the application with an error
 func Fatalf(format string, v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Fprintf(os.Stderr, format, v...)
 	os.Exit(1)
 }
 
 // Errorln logs a message to stderr
 func Errorln(v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Fprintln(os.Stderr, v...)
 }
 
 // Errorf logs a message to stderr
 func Errorf(format string, v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Fprintf(os.Stderr, format, v...)
 }
 
 // Infoln logs a message to stdout
 func Infoln(v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Println(v...)
 }
 
 // Infof logs a message to stdout
 func Infof(format string, v ...interface{}) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	fmt.Printf(format, v...)
 }
