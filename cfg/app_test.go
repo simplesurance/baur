1cbe4972c0fc447d860cd215bf1b8ff04da8450d
Fabian Holler <fabian.holler@simplesurance.de>
2018-06-11 16:51:02 +0200

    cfg: add tests that validate an example conf read from a file
    Validating an Example CFG might be succesful if we the value returned
    from ExampleRepository() and ExampleApp() but might fail after the same
    object was serialized to toml, written to a file and then read.
    This can happen if the toml serialization works different then expected.
    Add tests for those cases.

--- cfg/app_test.go
+++ cfg/app_test.go
@@ -1,10 +1,43 @@
 package cfg
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func Test_ExampleApp_IsValid(t *testing.T) {
 	a := ExampleApp("shop")
 	if err := a.Validate(); err != nil {
 		t.Error("example app conf fails validation: ", err)
+	}
+}
+
+func Test_ExampleApp_WrittenAndReadCfgIsValid(t *testing.T) {
+	tmpfileFD, err := ioutil.TempFile("", "baur")
+	if err != nil {
+		t.Fatal("opening tmpfile failed: ", err)
+	}
+
+	tmpfileName := tmpfileFD.Name()
+	tmpfileFD.Close()
+	os.Remove(tmpfileName)
+
+	a := ExampleApp("shop")
+	if err := a.Validate(); err != nil {
+		t.Error("example conf fails validation: ", err)
+	}
+
+	if err := a.ToFile(tmpfileName); err != nil {
+		t.Fatal("writing conf to file failed: ", err)
+	}
+
+	rRead, err := AppFromFile(tmpfileName)
+	if err != nil {
+		t.Fatal("reading conf from file failed: ", err)
+	}
+
+	if err := rRead.Validate(); err != nil {
+		t.Error("validating conf from file failed: ", err)
 	}
 }
--- cfg/repository_test.go
+++ cfg/repository_test.go
@@ -1,10 +1,43 @@
 package cfg
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func Test_ExampleRepository_IsValid(t *testing.T) {
 	r := ExampleRepository()
 	if err := r.Validate(); err != nil {
 		t.Error("example repository conf fails validation: ", err)
+	}
+}
+
+func Test_ExampleRepository_WrittenAndReadCfgIsValid(t *testing.T) {
+	tmpfileFD, err := ioutil.TempFile("", "baur")
+	if err != nil {
+		t.Fatal("opening tmpfile failed: ", err)
+	}
+
+	tmpfileName := tmpfileFD.Name()
+	tmpfileFD.Close()
+	defer os.Remove(tmpfileName)
+
+	r := ExampleRepository()
+	if err := r.Validate(); err != nil {
+		t.Error("example conf fails validation: ", err)
+	}
+
+	if err := r.ToFile(tmpfileName, true); err != nil {
+		t.Fatal("writing conf to file failed: ", err)
+	}
+
+	rRead, err := RepositoryFromFile(tmpfileName)
+	if err != nil {
+		t.Fatal("reading conf from file failed: ", err)
+	}
+
+	if err := rRead.Validate(); err != nil {
+		t.Error("validating conf from file failed: ", err)
 	}
 }
