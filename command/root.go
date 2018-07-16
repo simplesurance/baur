8258d476ae7c724691af00c9f53652144b6554f8
Fabian Holler <fabian.holler@simplesurance.de>
2018-07-16 12:08:30 +0200

    baur: add --cpu-prof parameter
    When --cpu-prof is passed, the go CPU profiler is activated and the
    results are written to a file in the temp directory.
    It's analyze and improve the performance

--- command/root.go
+++ command/root.go
@@ -1,6 +1,11 @@
 package command
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime/pprof"
+
 	"github.com/simplesurance/baur/log"
 	"github.com/simplesurance/baur/version"
 	"github.com/spf13/cobra"
@@ -14,16 +19,37 @@ var rootCmd = &cobra.Command{
 }
 
 var verboseFlag bool
+var cpuProfilingFlag bool
+
+var defCPUProfFile = filepath.Join(os.TempDir(), "baur-cpu.prof")
 
 func initSb(_ *cobra.Command, _ []string) {
 	log.DebugEnabled = verboseFlag
+
+	if cpuProfilingFlag {
+		cpuProfFile, err := os.Create(defCPUProfFile)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if err := pprof.StartCPUProfile(cpuProfFile); err != nil {
+			log.Fatalln(err)
+		}
+	}
 }
 
 // Execute parses commandline flags and execute their actions
 func Execute() {
 	rootCmd.PersistentFlags().BoolVarP(&verboseFlag, "verbose", "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolVar(&cpuProfilingFlag, "cpu-prof", false,
+		fmt.Sprintf("enable cpu profiling, result is written to %q", defCPUProfFile))
 
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalln(err)
+	}
+
+	if cpuProfilingFlag {
+		log.Infof("\ncpu profile written to %q\n", defCPUProfFile)
+		pprof.StopCPUProfile()
 	}
 }
