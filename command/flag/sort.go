8bcce156841572b3b86b81fd9bf682d436422bcd
Fabian Holler <fabian.holler@simplesurance.de>
2018-10-16 10:11:25 +0200

    command: refactor sort flag
    The sort flag is generalized to handle arbitrary field values.
    Previously the supported field values were hardcoded in the flag
    implementation.
    Now a mapping of commandline parameters to storage.Fields that the flag
    should support be passed in the constructor.
    
    The flag now also creates a usage output string via the Usage() method.
    String() is changed to return an empty string, cobra calls the function
    to show the default value in the usage output.
    It was shown as: "(default FieldUndefined-invalid)".
    When String() returns an empty string the whole default part is omitted.

--- command/flag/sort.go
+++ command/flag/sort.go
@@ -8,46 +8,72 @@ import (
 	"github.com/simplesurance/baur/storage"
 )
 
-// SortFlagFormatDescr contains the format description of SortFlag values
-const SortFlagFormatDescr = "<FIELD>-<ORDER>"
-
-// SortFlagValue implements pflag.Value
-type SortFlagValue struct {
-	storage.Sorter
+// Sort is a commandline flag to specify via which field and in which order
+// output should be sorted
+type Sort struct {
+	Value       storage.Sorter
+	validFields map[string]storage.Field
 }
 
-// String returns the Field and Order as string
-func (s *SortFlagValue) String() string {
-	return fmt.Sprintf("%s-%s", s.Field, s.Order)
+// NewSort returns a Sort flag.
+// The keys in the validFields maps are accepted as value for
+// Fields and map to the storage.Field values.
+func NewSort(validFields map[string]storage.Field) *Sort {
+	return &Sort{
+		validFields: validFields,
+	}
 }
 
-// Set parses the passed string and sets the SortFlagValue
-func (s *SortFlagValue) Set(sortStr string) error {
+// String returns the default value in the usage output
+func (s *Sort) String() string {
+	return ""
+}
+
+// Set parses the passed string and sets the Sort
+func (s *Sort) Set(sortStr string) error {
 	var err error
 
 	pieces := strings.Split(sortStr, "-")
 	if len(pieces) != 2 {
-		return fmt.Errorf("format must be %s", SortFlagFormatDescr)
+		return fmt.Errorf("format must be %s", s.Type())
 	}
 
-	switch strings.ToLower(pieces[0]) {
-	case "time":
-		s.Field = storage.FieldBuildStartTime
-	case "duration":
-		s.Field = storage.FieldBuildDuration
-	default:
-		return errors.New("field must be \"time\" or \"duration\"")
+	field, exist := s.validFields[strings.ToLower(pieces[0])]
+	if !exist {
+		return errors.New("format must be: " + s.Type())
 	}
 
-	s.Order, err = storage.OrderFromStr(pieces[1])
+	order, err := storage.OrderFromStr(pieces[1])
 	if err != nil {
 		return err
 	}
 
+	s.Value.Field = field
+	s.Value.Order = order
+
 	return nil
 }
 
-// Type returns the value description string
-func (*SortFlagValue) Type() string {
-	return SortFlagFormatDescr
+// Type returns the format description
+func (s *Sort) Type() string {
+	return "<FIELD>-<ORDER>"
+}
+
+// Usage returns a usage description, important parts are passed through
+// highlightFn
+func (s *Sort) Usage(highlightFn func(a ...interface{}) string) string {
+	fields := make([]string, 0, len(s.validFields))
+
+	for k := range s.validFields {
+		fields = append(fields, highlightFn(k))
+	}
+
+	return strings.TrimSpace(fmt.Sprintf(`
+Sort the list by a specific field.
+Format: %s
+where %s is one of: %s,
+and %s one of: %s, %s`,
+		highlightFn(s.Type()),
+		highlightFn("FIELD"), strings.Join(fields, ", "),
+		highlightFn("<ORDER>"), highlightFn(storage.OrderAsc.String()), highlightFn(storage.OrderDesc.String())))
 }
