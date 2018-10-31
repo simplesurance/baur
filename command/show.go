a150be65cf4746d763214b4e51fb25f71fbce011
Fabian Holler <fabian.holler@simplesurance.de>
2018-10-31 12:20:31 +0100

    command: merge show app and show build into 1x command
    Revert splitting the show command into 2x subcommands again.
    Now there is again only "baur show" which can be passed an app name, app
    path or build id

--- command/show.go
+++ command/show.go
@@ -1,14 +1,159 @@
 package command
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/spf13/cobra"
+
+	"github.com/simplesurance/baur/format"
+	"github.com/simplesurance/baur/format/table"
+	"github.com/simplesurance/baur/log"
+	"github.com/simplesurance/baur/storage"
 )
 
+const showLongHelp = `
+Show information about an application or a build.
+
+If the name or the path to an application directory is passed,
+application information are shown.
+If a numeric build ID is passed, information about the build are shown.
+`
+
+const showExamples = `
+baur show calc		show information about the calc application
+baur show ui/shop	show information about the app in the ui/shop directory
+baur show 512		show information about build 512
+`
+
 var showCmd = &cobra.Command{
-	Use:   "show",
-	Short: "show information about apps, builds, inputs",
+	Use:     "show APP|APP-PATH|BUILD-ID",
+	Short:   "show information about apps or builds",
+	Args:    cobra.ExactArgs(1),
+	Run:     show,
+	Long:    strings.TrimSpace(showLongHelp),
+	Example: strings.TrimSpace(showExamples),
 }
 
 func init() {
 	rootCmd.AddCommand(showCmd)
+}
+
+func show(cmd *cobra.Command, args []string) {
+	buildID, err := strconv.Atoi(args[0])
+	if err == nil {
+		showBuild(buildID)
+	} else {
+		showApp(args[0])
+	}
+}
+
+func showApp(arg string) {
+	var formatter format.Formatter
+
+	repo := MustFindRepository()
+	app := mustArgToApp(repo, arg)
+
+	formatter = table.New(nil, os.Stdout)
+
+	mustWriteRow(formatter, []interface{}{underline("General:")})
+	mustWriteRow(formatter, []interface{}{"", "Name:", highlight(app.Name)})
+	mustWriteRow(formatter, []interface{}{"", "Path:", highlight(app.RelPath)})
+	mustWriteRow(formatter, []interface{}{"", "Build Command:", highlight(app.BuildCmd)})
+
+	if len(app.Outputs) != 0 {
+		mustWriteRow(formatter, []interface{}{})
+		mustWriteRow(formatter, []interface{}{underline("Outputs:")})
+
+		for i, art := range app.Outputs {
+			mustWriteRow(formatter, []interface{}{"", "Type:", highlight(art.Type())})
+			mustWriteRow(formatter, []interface{}{"", "Local:", highlight(art.String())})
+			mustWriteRow(formatter, []interface{}{"", "Remote:", highlight(art.UploadDestination())})
+
+			if i+1 < len(app.Outputs) {
+				mustWriteRow(formatter, []interface{}{})
+			}
+		}
+	}
+
+	if len(app.BuildInputPaths) != 0 {
+		mustWriteRow(formatter, []interface{}{})
+		mustWriteRow(formatter, []interface{}{underline("Inputs:")})
+
+		for i, bi := range app.BuildInputPaths {
+			mustWriteRow(formatter, []interface{}{"", "Type:", highlight(bi.Type())})
+			mustWriteRow(formatter, []interface{}{"", "Configuration:", highlight(bi.String())})
+
+			if i+1 < len(app.BuildInputPaths) {
+				mustWriteRow(formatter, []interface{}{})
+			}
+		}
+
+	}
+
+	if err := formatter.Flush(); err != nil {
+		log.Fatalln(err)
+	}
+}
+func showBuild(buildID int) {
+	var formatter format.Formatter
+
+	repo := MustFindRepository()
+	storageClt := MustGetPostgresClt(repo)
+
+	build, err := storageClt.GetBuildWithoutInputs(int(buildID))
+	if err != nil {
+		if err == storage.ErrNotExist {
+			log.Fatalf("build with id %d does not exist\n", buildID)
+		}
+
+		log.Fatalln(err)
+	}
+
+	formatter = table.New(nil, os.Stdout)
+
+	mustWriteRow(formatter, []interface{}{underline("General:")})
+	mustWriteRow(formatter, []interface{}{"", "Name:", highlight(build.Application.Name)})
+	mustWriteRow(formatter, []interface{}{"", "ID:", highlight(build.ID)})
+
+	mustWriteRow(formatter, []interface{}{"", "Started At:", highlight(build.StartTimeStamp)})
+	mustWriteRow(formatter, []interface{}{
+		"",
+		"Duration:",
+		highlight(fmt.Sprintf("%.2f s", build.StopTimeStamp.Sub(build.StartTimeStamp).Seconds())),
+	})
+
+	mustWriteRow(formatter, []interface{}{"", "Git Commit:", highlight(vcsStr(&build.VCSState))})
+
+	mustWriteRow(formatter, []interface{}{"", "Total Input Digest:", highlight(build.TotalInputDigest)})
+
+	if len(build.Outputs) > 0 {
+		mustWriteRow(formatter, []interface{}{})
+		mustWriteRow(formatter, []interface{}{underline("Outputs:")})
+	}
+	for i, o := range build.Outputs {
+		mustWriteRow(formatter, []interface{}{"", "Type:", highlight(o.Type)})
+		mustWriteRow(formatter, []interface{}{"", "URI:", highlight(o.Upload.URI)})
+		mustWriteRow(formatter, []interface{}{"", "Digest:", highlight(o.Digest)})
+		mustWriteRow(formatter, []interface{}{
+			"",
+			"Size:",
+			highlight(fmt.Sprintf("%.2f MiB", float64(o.SizeBytes)/1024/1024)),
+		})
+		mustWriteRow(formatter, []interface{}{
+			"",
+			"Upload Duration:",
+			highlight(fmt.Sprintf("%.2f s", o.Upload.UploadDuration.Seconds())),
+		})
+		if i+1 < len(build.Outputs) {
+			mustWriteRow(formatter, []interface{}{})
+		}
+	}
+
+	if err := formatter.Flush(); err != nil {
+		log.Fatalln(err)
+	}
+
 }
