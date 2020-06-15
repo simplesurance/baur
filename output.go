dd2078a87ea1d00b007d8404e4fdaba0270e2f8a
Fabian Holler <fabian.holler@simplesurance.de>
2020-06-15 19:01:28 +0200

    refactor OutputsFromTask()

--- output.go
+++ output.go
@@ -35,10 +35,8 @@ type Output interface {
 	Type() OutputType
 }
 
-func OutputsFromTask(dockerClient DockerInfoClient, task *Task) ([]Output, error) {
-	var result []Output
-
-	// TODO: move each loop to an own sub-function
+func dockerOutputs(dockerClient DockerInfoClient, task *Task) ([]Output, error) {
+	result := make([]Output, 0, len(task.Outputs.DockerImage))
 
 	for _, dockerOutput := range task.Outputs.DockerImage {
 		d, err := NewOutputDockerImageFromIIDFile(
@@ -59,6 +57,12 @@ func OutputsFromTask(dockerClient DockerInfoClient, task *Task) ([]Output, error
 		result = append(result, d)
 	}
 
+	return result, nil
+}
+
+func fileOutputs(task *Task) ([]Output, error) {
+	result := make([]Output, 0, len(task.Outputs.File))
+
 	for _, fileOutput := range task.Outputs.File {
 		var s3Upload *UploadInfoS3
 		var fileCopyUpload *UploadInfoFileCopy
@@ -68,7 +72,6 @@ func OutputsFromTask(dockerClient DockerInfoClient, task *Task) ([]Output, error
 		}
 
 		// TODO: use pointers in the outputfile struct for filecopy and S3 instead of having to provide and use IsEmpty)
-
 		if !fileOutput.S3Upload.IsEmpty() {
 			s3Upload = &UploadInfoS3{
 				Bucket: fileOutput.S3Upload.Bucket,
@@ -87,6 +90,26 @@ func OutputsFromTask(dockerClient DockerInfoClient, task *Task) ([]Output, error
 			fileCopyUpload,
 		))
 	}
+
+	return result, nil
+}
+
+// OutputsFromTask returns the Outputs that running the task produces.
+// If the outputs do not exist, the function might fail.
+func OutputsFromTask(dockerClient DockerInfoClient, task *Task) ([]Output, error) {
+	dockerImages, err := dockerOutputs(dockerClient, task)
+	if err != nil {
+		return nil, err
+	}
+
+	files, err := fileOutputs(task)
+	if err != nil {
+		return nil, err
+	}
+
+	result := make([]Output, 0, len(dockerImages)+len(files))
+	result = append(result, dockerImages...)
+	result = append(result, files...)
 
 	return result, nil
 }
