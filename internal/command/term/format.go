b616dc25a5c6809df05ed01692d7949e687004fb
Fabian Holler <fabian.holler@simplesurance.de>
2020-06-25 15:54:28 +0200

    term: add option parameter to format in base unit without unit name suffix
    FormatSize() and FormatDuration() now accepts an optional list of Options.
    The only available option for now is FormatBaseWithoutUnitName(enabled).
    When true is passed, the format functions return the value in it's base unit
    without unit name suffix.
    
    This is handy for formatting units differently when --csv is passed on the
    commandline.

--- internal/command/term/format.go
+++ internal/command/term/format.go
@@ -5,7 +5,34 @@ import (
 	"time"
 )
 
-func FormatSize(bytes uint64) string {
+type FormatOption func(*fmtSettings)
+
+type fmtSettings struct {
+	baseUnitWithoutUnitName bool
+}
+
+// FormatBaseWithoutUnitName when enabled the Format functions return the value
+// in it's base unit without unit-name suffix
+func FormatBaseWithoutUnitName(enable bool) FormatOption {
+	return func(o *fmtSettings) {
+		o.baseUnitWithoutUnitName = enable
+	}
+}
+
+func evalOptions(opts []FormatOption) *fmtSettings {
+	var s fmtSettings
+
+	for _, opt := range opts {
+		opt(&s)
+	}
+
+	return &s
+}
+func FormatSize(bytes uint64, opts ...FormatOption) string {
+	if evalOptions(opts).baseUnitWithoutUnitName {
+		return fmt.Sprint(bytes)
+	}
+
 	if bytes < 1024 {
 		return fmt.Sprintf("%d B", bytes)
 	}
@@ -21,7 +48,11 @@ func FormatSize(bytes uint64) string {
 	return fmt.Sprintf("%.3f GiB", float64(bytes)/1024/1024/1024)
 }
 
-func FormatDuration(d time.Duration) string {
+func FormatDuration(d time.Duration, opts ...FormatOption) string {
+	if evalOptions(opts).baseUnitWithoutUnitName {
+		return fmt.Sprintf("%.3f", d.Seconds())
+	}
+
 	if d.Minutes() > 1 {
 		return d.Round(time.Second).String()
 	}
