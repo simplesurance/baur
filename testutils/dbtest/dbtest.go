1c1200f92b4956e6efc9ff85b68b469bbd59091e
Fabian Holler <fabian.holler@simplesurance.de>
2020-07-13 15:53:37 +0200

    test: use different env variable to specify test psql database uri
    BAUR_POSTGRESQL_URL is also used by the baur commands.
    This caused that when a testcase created a baur repository config with a
    postgresql URL specifying another db, the value from the config was not used
    because it was overwritten by the BAUR_POSTGRESQL_URL env variable.

--- testutils/dbtest/dbtest.go
+++ testutils/dbtest/dbtest.go
@@ -10,10 +10,10 @@ import (
 
 const defPSQLURL = "postgres://postgres@localhost:5434/baur?sslmode=disable&connect_timeout=5"
 
-// DDBURL returns the value of the environment variable BAUR_POSTGRESQL_URL.
+// DDBURL returns the value of the environment variable BAUR_TEST_POSTGRESQL_URL.
 // If the environment variable is undefined or empty, defDBURL is returned.
 func PSQLURL() string {
-	if url := os.Getenv("BAUR_POSTGRESQL_URL"); url != "" {
+	if url := os.Getenv("BAUR_TEST_POSTGRESQL_URL"); url != "" {
 		return url
 	}
 
