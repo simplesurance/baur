ad134258be65c47895db060e0f038f63ab4c1250
Fabian Holler <fabian.holler@simplesurance.de>
2020-08-25 10:12:02 +0200

    command: add a "upgrade configs" testcase
    The testcase checks out the example git repo with configs for baur 0.18.0,
    runs "baur ugprade configs" and checks if all expected tasks can be found in the
    repository.
    
    It should be extended to also run the tasks and check if they are defined
    equally to before.

--- internal/command/command_test.go
+++ internal/command/command_test.go
@@ -3,7 +3,6 @@
 package command
 
 import (
-	"bytes"
 	"encoding/csv"
 	"strings"
 	"testing"
@@ -13,66 +12,67 @@ import (
 
 	"github.com/simplesurance/baur/v1"
 	"github.com/simplesurance/baur/v1/exec"
-	"github.com/simplesurance/baur/v1/internal/command/term"
 	"github.com/simplesurance/baur/v1/internal/testutils/gittest"
 	"github.com/simplesurance/baur/v1/internal/testutils/repotest"
 )
-
-func initTest(t *testing.T) {
-	t.Helper()
-
-	exitFunc = func(code int) {
-		t.Fatalf("baur command exited with code %d", code)
-	}
-
-	exec.DefaultDebugfFn = t.Logf
-}
-
-// baurCSVStatus runs "baur status --csv" and returns a slice where each
-// element is a slice of csv fields per line
-func baurCSVStatus(t *testing.T) [][]string {
-	var stdoutBuf bytes.Buffer
-
-	t.Helper()
-
-	stdout = term.NewStream(&stdoutBuf)
-
-	statusCmd := newStatusCmd()
-	statusCmd.csv = true
-
-	statusCmd.Command.Run(&statusCmd.Command, nil)
-
-	statusOut, err := csv.NewReader(&stdoutBuf).ReadAll()
-	require.NoError(t, err)
-
-	return statusOut
-}
-
-// baurCSVLsApps runs "baur ls apps --csv" and returns a slice where each
-// element is a slice of csv fields per line
-func baurCSVLsApps(t *testing.T) [][]string {
-	var stdoutBuf bytes.Buffer
-
-	t.Helper()
-
-	stdout = term.NewStream(&stdoutBuf)
-
-	lsAppsCmd := newLsAppsCmd()
-	lsAppsCmd.csv = true
-
-	lsAppsCmd.Command.Run(&lsAppsCmd.Command, nil)
-
-	statusOut, err := csv.NewReader(&stdoutBuf).ReadAll()
-	require.NoError(t, err)
-
-	return statusOut
-}
 
 func runInitDb(t *testing.T) {
 	t.Helper()
 
 	t.Log("creating database schema")
 	initDb(initDbCmd, nil)
+}
+
+// baurCSVLsApps runs "baur ls apps --csv" and returns a slice where each
+// element is a slice of csv fields per line
+func baurCSVLsApps(t *testing.T) [][]string {
+	t.Helper()
+
+	stdoutBuf, _ := interceptCmdOutput()
+
+	lsAppsCmd := newLsAppsCmd()
+	lsAppsCmd.csv = true
+
+	lsAppsCmd.Command.Run(&lsAppsCmd.Command, nil)
+
+	statusOut, err := csv.NewReader(stdoutBuf).ReadAll()
+	require.NoError(t, err)
+
+	return statusOut
+}
+
+type csvStatus struct {
+	taskID string
+	status string
+	commit string
+}
+
+// baurCSVStatus runs "baur status --csv" and returns the result.
+func baurCSVStatus(t *testing.T) []*csvStatus {
+	t.Helper()
+
+	stdoutBuf, _ := interceptCmdOutput()
+
+	statusCmd := newStatusCmd()
+	statusCmd.csv = true
+
+	statusCmd.Command.Run(&statusCmd.Command, nil)
+
+	statusOut, err := csv.NewReader(stdoutBuf).ReadAll()
+	require.NoError(t, err)
+
+	result := make([]*csvStatus, 0, len(statusOut))
+
+	for _, line := range statusOut {
+		require.Equal(t, 5, len(line))
+		result = append(result, &csvStatus{
+			taskID: line[0],
+			status: line[3],
+			commit: line[4],
+		})
+	}
+
+	return result
 }
 
 // TestRunningPendingTasksChangesStatus creates a new baur repository with 2
@@ -123,17 +123,11 @@ func TestRunningPendingTasksChangesStatus(t *testing.T) {
 			statusOut := baurCSVStatus(t)
 
 			var taskIds []string
-			for _, line := range statusOut {
-				require.Equal(t, 5, len(line))
-				taskID := line[0]
-				status := line[3]
-				commit := line[4]
+			for _, task := range statusOut {
+				taskIds = append(taskIds, task.taskID)
 
-				taskIds = append(taskIds, taskID)
-
-				assert.Equal(t, status, baur.TaskStatusExecutionPending.String(), "status is not %s: %q", baur.TaskStatusExecutionPending, line)
-
-				assert.Equal(t, "", commit)
+				assert.Equal(t, baur.TaskStatusExecutionPending.String(), task.status)
+				assert.Equal(t, "", task.commit)
 			}
 			assert.ElementsMatch(t, taskIds, r.TaskIDs(), "baur status is missing some tasks")
 
@@ -142,18 +136,13 @@ func TestRunningPendingTasksChangesStatus(t *testing.T) {
 
 			statusOut = baurCSVStatus(t)
 			taskIds = nil
-			for _, line := range statusOut {
-				require.Equal(t, 5, len(line))
-				taskID := line[0]
-				status := line[3]
-				commit := line[4]
+			for _, task := range statusOut {
+				taskIds = append(taskIds, task.taskID)
 
-				taskIds = append(taskIds, taskID)
-
-				assert.Equal(t, status, baur.TaskStatusRunExist.String(), "status is not %s: %q", baur.TaskStatusRunExist, line)
+				assert.Equal(t, baur.TaskStatusRunExist.String(), task.status)
 
 				if tc.withGitRepository {
-					assert.Equal(t, headCommit, commit)
+					assert.Equal(t, headCommit, task.commit)
 				}
 			}
 			assert.ElementsMatch(t, taskIds, r.TaskIDs(), "baur status is missing some tasks")
---
+++ internal/command/util_test.go
@@ -0,0 +1,34 @@
+package command
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/simplesurance/baur/v1/exec"
+	"github.com/simplesurance/baur/v1/internal/command/term"
+)
+
+// interceptCmdOutput changes the stdout and stderr streams to that the
+// commands write to the returned buffers
+func interceptCmdOutput() (stdoutBuf, stderrBuf *bytes.Buffer) {
+	var bufStdout bytes.Buffer
+	var bufStderr bytes.Buffer
+
+	stdout = term.NewStream(&bufStdout)
+	stderr = term.NewStream(&bufStderr)
+
+	return &bufStdout, &bufStderr
+}
+
+// initTest does the following:
+// - changes the exitFunc to fail the testcase when it is called with an exitCode !=0.
+// - changes the exec debug function to the test logger,
+func initTest(t *testing.T) {
+	t.Helper()
+
+	exitFunc = func(code int) {
+		t.Fatalf("baur command exited with code %d", code)
+	}
+
+	exec.DefaultDebugfFn = t.Logf
+}
