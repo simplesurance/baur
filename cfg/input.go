3bfd325901a0639c24c49ab0895d59e29cf4209c
Fabian Holler <fabian.holler@simplesurance.de>
2020-09-08 18:29:58 +0200

    cfg: convert the GolangSources field in the app config to a slice
    The GolangSources field consisted of multiple field and app configs only had a
    single GolangSources element.
    
    When includes that contained GolangSources definitions were merged with the app
    config, the values of the includes were appended to the single GolangSources
    definition.
    This could cause that golangsources were evaluated for a path with wrong
    Environment, BuildFlags or Tests settings.
    
    Convert the GolangSources to a slice.
    This allows to keep the settings from GolangSources from includes and also
    allows to specify multiple GolangSources definitions with different settings in
    the .app.toml file.

--- cfg/input.go
+++ cfg/input.go
@@ -1,12 +1,14 @@
 package cfg
 
-import "github.com/simplesurance/baur/v1/cfg/resolver"
+import (
+	"github.com/simplesurance/baur/v1/cfg/resolver"
+)
 
 // Input contains information about task inputs
 type Input struct {
-	Files         FileInputs    `comment:"Inputs specified by file glob paths"`
-	GitFiles      GitFileInputs `comment:"Inputs specified by path, matching only Git tracked files"`
-	GolangSources GolangSources `comment:"Inputs specified by resolving dependencies of Golang source files or packages."`
+	Files         FileInputs      `comment:"Inputs specified by file glob paths"`
+	GitFiles      GitFileInputs   `comment:"Inputs specified by path, matching only Git tracked files"`
+	GolangSources []GolangSources `comment:"Inputs specified by resolving dependencies of Golang source files or packages."`
 }
 
 func (in *Input) FileInputs() *FileInputs {
@@ -17,15 +19,15 @@ func (in *Input) GitFileInputs() *GitFileInputs {
 	return &in.GitFiles
 }
 
-func (in *Input) GolangSourcesInputs() *GolangSources {
-	return &in.GolangSources
+func (in *Input) GolangSourcesInputs() []GolangSources {
+	return in.GolangSources
 }
 
 // Merge appends the information in other to in.
 func (in *Input) Merge(other InputDef) {
 	in.Files.Merge(other.FileInputs())
 	in.GitFiles.Merge(other.GitFileInputs())
-	in.GolangSources.Merge(other.GolangSourcesInputs())
+	in.GolangSources = append(in.GolangSources, other.GolangSourcesInputs()...)
 }
 
 func (in *Input) Resolve(resolvers resolver.Resolver) error {
@@ -37,8 +39,12 @@ func (in *Input) Resolve(resolvers resolver.Resolver) error {
 		return FieldErrorWrap(err, "Gitfiles")
 	}
 
-	if err := in.GolangSources.Resolve(resolvers); err != nil {
-		return FieldErrorWrap(err, "GoLangSources")
+	for i, gs := range in.GolangSources {
+		if err := gs.Resolve(resolvers); err != nil {
+			return FieldErrorWrap(err, "GoLangSources")
+		}
+
+		in.GolangSources[i] = gs
 	}
 
 	return nil
@@ -50,8 +56,10 @@ func InputValidate(i InputDef) error {
 		return FieldErrorWrap(err, "Files")
 	}
 
-	if err := i.GolangSourcesInputs().Validate(); err != nil {
-		return FieldErrorWrap(err, "GolangSources")
+	for _, gs := range i.GolangSourcesInputs() {
+		if err := gs.Validate(); err != nil {
+			return FieldErrorWrap(err, "GolangSources")
+		}
 	}
 
 	// TODO: add validation for gitfiles section
--- cfg/inputdef.go
+++ cfg/inputdef.go
@@ -3,12 +3,12 @@ package cfg
 type InputDef interface {
 	FileInputs() *FileInputs
 	GitFileInputs() *GitFileInputs
-	GolangSourcesInputs() *GolangSources
+	GolangSourcesInputs() []GolangSources
 }
 
 // InputsAreEmpty returns true if no inputs are defined
 func InputsAreEmpty(in InputDef) bool {
 	return len(in.FileInputs().Paths) == 0 &&
 		len(in.GitFileInputs().Paths) == 0 &&
-		in.GolangSourcesInputs().IsEmpty()
+		len(in.GolangSourcesInputs()) == 0
 }
