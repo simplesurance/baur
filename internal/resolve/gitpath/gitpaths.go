49ca0b8a837f8226d6b68d6d1aa880ad5f7a30df
Fabian Holler <fabian.holler@simplesurance.de>
2021-01-12 18:03:55 +0100

    resolver: use same glob syntax for Glob and GitPath resolver
    The Glob and GitPath resolvers were processing patterns differently.
    For example when a '*' was used as path in the Input.GitFiles section it also matched
    directory separators, when '*' was used as path in the Input.Files section it
    did not match directory separators.
    
    Ensure paths are resolved the same by resolving globs for both resolvers via fs.Glob().
    The GitPath resolver is then passing the paths to "git ls-files" to check if
    they are committed to the repository. The "--noglob-pathspecs" parameter is
    passed to "git ls-files" to treat all paths literally.

--- internal/resolve/gitpath/gitpaths.go
+++ internal/resolve/gitpath/gitpaths.go
@@ -1,6 +1,8 @@
 package gitpath
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 	"strings"
 
@@ -17,13 +19,36 @@ type Resolver struct{}
 // workingDir must be a directory that is part of a Git repository.
 // If a resolved file does not exist an error is returned.
 func (r *Resolver) Resolve(workingDir string, errorUnmatch bool, globs ...string) ([]string, error) {
-	if len(globs) == 0 {
-		return []string{}, nil
+	var resolvedPaths []string
+
+	for _, glob := range globs {
+		absGlob := filepath.Join(workingDir, glob)
+		// Globs are resolved via the same method then resolve/files
+		// uses. This ensures the same pattern resolves in the same way
+		// for both. git ls-files resolves '*` differently, it also matches directory seperators.
+		paths, err := fs.FileGlob(absGlob)
+		if err != nil {
+			return nil, fmt.Errorf("resolving %q failed: %w", absGlob, err)
+		}
+
+		if len(paths) == 0 {
+			if errorUnmatch {
+				return nil, fmt.Errorf("%q did not match any files: %w", absGlob, os.ErrNotExist)
+			}
+
+			continue
+		}
+
+		resolvedPaths = append(resolvedPaths, paths...)
 	}
 
-	out, err := git.LsFiles(workingDir, errorUnmatch, globs...)
+	if len(resolvedPaths) == 0 {
+		return resolvedPaths, nil
+	}
+
+	out, err := git.LsFiles(workingDir, resolvedPaths...)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("git ls-files failed: %w", err)
 	}
 
 	relPaths := strings.Split(out, "\n")
