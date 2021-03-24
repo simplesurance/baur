b8427517db2edb87abb570304f676b4c76ad45fe
kostyay <kostya.y@gmail.com>
2021-03-24 10:27:46 +0200

    Remove deprecation

--- pkg/baur/cfg_resolvers.go
+++ pkg/baur/cfg_resolvers.go
@@ -4,20 +4,9 @@ import (
 	"github.com/simplesurance/baur/v1/pkg/cfg/resolver"
 )
 
-const (
-	// bc convertions - in gotemplate functions are called without dot
-	uuidOldVarname = "{{ .uuid }}"
-	uuidNewVarname = "{{ uuid }}"
-
-	gitCommitOldVarname = "{{ .gitcommit }}"
-	gitCommitNewVarname = "{{ gitcommit }}"
-)
-
 // defaultAppCfgResolvers returns the default set of resolvers that is applied on application configs.
 func defaultAppCfgResolvers(rootPath, appName string, gitCommitFn func() (string, error)) resolver.Resolver {
 	return resolver.List{
-		&resolver.StrReplacement{Old: uuidOldVarname, New: uuidNewVarname},
-		&resolver.StrReplacement{Old: gitCommitOldVarname, New: gitCommitNewVarname},
 		resolver.NewGoTemplate(appName, rootPath, gitCommitFn),
 	}
 }
