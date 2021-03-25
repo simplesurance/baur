1f31a4fe243bf0a03e2ad35ae4f4c71291413893
kostyay <kostya.y@gmail.com>
2021-03-25 22:23:45 +0200

    Add gotemplate tests

--- pkg/cfg/resolver/gotemplate_test.go
+++ pkg/cfg/resolver/gotemplate_test.go
@@ -7,39 +7,85 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestEnvVarResolve(t *testing.T) {
+func TestResolve(t *testing.T) {
 	const envVar = "_baurTestEnvVar"
 	const envVarVal = "hello123"
-
-	testStr := fmt.Sprintf("test {{ env \"%s\" }} {{ env \"%s\" }}bye", envVar, envVar)
-	expectedResult := fmt.Sprintf("test %s %sbye", envVarVal, envVarVal)
-
-	r := NewGoTemplate("appname", "/tmp", func() (string, error) {
-		return "commit", nil
-	})
+	const rootDir = "/tmp/f00bar"
+	const commitID = "commit1231231231"
+	const appName = "my-app-name"
 
 	os.Setenv(envVar, envVarVal)
 	t.Cleanup(func() {
 		os.Unsetenv(envVar)
 	})
 
-	res, err := r.Resolve(testStr)
-	require.NoError(t, err)
-	require.Equal(t, expectedResult, res)
-}
+	testCases := []struct {
+		name           string
+		input          string
+		expectedResult string
+		validator      func(string) error
+	}{
+		{
+			"Test env",
+			fmt.Sprintf("test {{ env \"%s\" }} {{ env \"%s\" }}bye", envVar, envVar),
+			fmt.Sprintf("test %s %sbye", envVarVal, envVarVal),
+			nil,
+		},
+		{
+			"Test root",
+			"{{ .root }}",
+			rootDir,
+			nil,
+		},
+		{
+			"Test appname",
+			"{{ .appname }}",
+			appName,
+			nil,
+		},
+		{
+			"Test commit",
+			"{{ gitcommit }}",
+			commitID,
+			nil,
+		},
+		{
+			"Test {{ uuid }}",
+			"{{ uuid }}",
+			"",
+			func(result string) error {
+				matched, err := regexp.MatchString(`(?i)^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$`, result)
+				if err != nil {
+					return err
+				}
 
-func TestUUIDVar(t *testing.T) {
-	r := NewGoTemplate("appname", "/tmp", func() (string, error) {
-		return "commit", nil
+				if matched != true {
+					return fmt.Errorf("string didnt match")
+				}
+
+				return nil
+			},
+		},
+	}
+
+	subject := NewGoTemplate(appName, rootDir, func() (string, error) {
+		return commitID, nil
 	})
 
-	result, err := r.Resolve("{{ uuid }}")
-	assert.NoError(t, err)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(tt *testing.T) {
+			result, err := subject.Resolve(tc.input)
+			assert.NoError(t, err)
 
-	matched, err := regexp.MatchString(`(?i)^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$`, result)
-	assert.NoError(t, err)
-	assert.True(t, matched)
+			if tc.expectedResult != "" {
+				assert.Equal(tt, tc.expectedResult, result)
+			}
+
+			if tc.validator != nil {
+				assert.NoError(tt, tc.validator(result))
+			}
+		})
+	}
 }
