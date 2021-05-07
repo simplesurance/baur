d2e09f2bcec161e65046fd8a95c7d1726f094e91
Fabian Holler <fabian.holler@simplesurance.de>
2021-05-07 13:21:34 +0200

    tests: check in TestRunSimultaneously also if parallel task processes exist
    Extend the TestRunSimultaneously testcase to also check if the processes of the
    parallel task that should be executed are running.
    
    This ensures that baur also really runs the task in parallel.

--- internal/command/run_test.go
+++ internal/command/run_test.go
@@ -4,6 +4,8 @@ package command
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,21 +17,79 @@ import (
 func TestRunSimultaneously(t *testing.T) {
 	initTest(t)
 
-	r := repotest.CreateBaurRepository(t, repotest.WithNewDB())
+	r := repotest.CreateBaurRepository(t, repotest.WithNewDB(), repotest.WithKeepTmpDir())
 
-	testApps := 6
-	apps := make([]*cfg.App, testApps)
+	parallelTaskCnt := 6
+	apps := make([]*cfg.App, parallelTaskCnt)
 
-	for i := 0; i < testApps; i++ {
+	var checkScript = []byte(`#!/usr/bin/env bash
+set -eu -o pipefail
+
+parallel_tasks="$1"
+max_iter=20
+process_found=0
+
+for (( i=0; i< $parallel_tasks; i++ )); do
+	for (( j=0; ; j++ )); do
+		# [c] is needed to exclude the grep process itself from the result
+		ps -x | grep -q "[c]heckscript${i}.sh" && {
+			echo "task $i is running"
+			break
+		}
+
+		if [ $j -eq $max_iter ]; then
+			echo "task $i not running"
+			exit 1
+		fi
+
+		sleep 0.5
+	done
+done
+
+# sleep a bit to give all parallel running processes a chance to find each other
+sleep 3
+`)
+
+	var tasks cfg.Tasks
+
+	for i := 0; i < parallelTaskCnt; i++ {
+		err := ioutil.WriteFile(
+			filepath.Join(r.Dir, fmt.Sprintf("checkscript%d.sh", i)),
+			checkScript,
+			0755,
+		)
+		require.NoError(t, err)
+
 		apps[i] = r.CreateAppWithNoOutputs(t, fmt.Sprintf("myapp-%d", i))
+
+		tasks = append(tasks, &cfg.Task{
+			Name: fmt.Sprintf("check%d", i),
+			Command: []string{
+				filepath.Join(r.Dir, fmt.Sprintf("checkscript%d.sh", i)),
+				fmt.Sprint(parallelTaskCnt),
+			},
+			Input: cfg.Input{
+				Files: []cfg.FileInputs{
+					{Paths: []string{".app.toml"}},
+				},
+			},
+		})
 	}
+
+	appCfg := cfg.App{
+		Name:  "testapp",
+		Tasks: tasks,
+	}
+
+	err := appCfg.ToFile(filepath.Join(r.Dir, ".app.toml"))
+	require.NoError(t, err)
 
 	doInitDb(t)
 
 	_, _ = interceptCmdOutput(t)
 
 	runCmdTest := newRunCmd()
-	runCmdTest.SetArgs([]string{"-p", "3"})
-	err := runCmdTest.Execute()
+	runCmdTest.SetArgs([]string{"-p", fmt.Sprint(parallelTaskCnt)})
+	err = runCmdTest.Execute()
 	require.NoError(t, err)
 }
