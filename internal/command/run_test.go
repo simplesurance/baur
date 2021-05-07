87f56db7a4c09f1f195e019d5c1587870642b177
Fabian Holler <fabian.holler@simplesurance.de>
2021-05-07 13:21:34 +0200

    tests: check in TestRunSimultaneously also if parallel task processes exist
    Extend the TestRunSimultaneously testcase to also check if the tasks are really
    executed in parallel.
    
    The parallel executed tasks run a shell script that writes their start and exist
    timestamp to a logfile.
    After "baur run" was executed, the tests checks if all timestamps of the
    executed tasks are in the same interval.

--- internal/command/run_test.go
+++ internal/command/run_test.go
@@ -4,6 +4,10 @@ package command
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -17,19 +21,98 @@ func TestRunSimultaneously(t *testing.T) {
 
 	r := repotest.CreateBaurRepository(t, repotest.WithNewDB())
 
-	testApps := 6
-	apps := make([]*cfg.App, testApps)
+	parallelTaskCnt := 6
+	apps := make([]*cfg.App, parallelTaskCnt)
 
-	for i := 0; i < testApps; i++ {
+	// checkScript is exected by the tasks, it logs the start and end
+	// timestamp of the script to file.
+	// The test checks if all task start/endtimestamps are in the same period.
+	var checkScript = []byte(`#!/usr/bin/env bash
+set -veu -o pipefail
+
+runtime_logfile="$1"
+
+date +%s > "$runtime_logfile"
+sleep 3
+date +%s >> "$runtime_logfile"
+`)
+
+	var tasks cfg.Tasks
+
+	var runtimeLogfiles []string
+
+	for i := 0; i < parallelTaskCnt; i++ {
+		err := ioutil.WriteFile(
+			filepath.Join(r.Dir, fmt.Sprintf("checkscript%d.sh", i)),
+			checkScript,
+			0755,
+		)
+		require.NoError(t, err)
+
 		apps[i] = r.CreateAppWithNoOutputs(t, fmt.Sprintf("myapp-%d", i))
+
+		logfile := filepath.Join(r.Dir, fmt.Sprintf("runtimelog-task-%d", i))
+		runtimeLogfiles = append(runtimeLogfiles, logfile)
+
+		tasks = append(tasks, &cfg.Task{
+			Name: fmt.Sprintf("check%d", i),
+			Command: []string{
+				"bash",
+				filepath.Join(r.Dir, fmt.Sprintf("checkscript%d.sh", i)),
+				logfile,
+			},
+			Input: cfg.Input{
+				Files: []cfg.FileInputs{
+					{Paths: []string{".app.toml"}},
+				},
+			},
+		})
 	}
+
+	appCfg := cfg.App{
+		Name:  "testapp",
+		Tasks: tasks,
+	}
+
+	err := appCfg.ToFile(filepath.Join(r.Dir, ".app.toml"))
+	require.NoError(t, err)
 
 	doInitDb(t)
 
-	_, _ = interceptCmdOutput(t)
-
 	runCmdTest := newRunCmd()
-	runCmdTest.SetArgs([]string{"-p", "3"})
-	err := runCmdTest.Execute()
+	runCmdTest.SetArgs([]string{"-p", fmt.Sprint(parallelTaskCnt)})
+	err = runCmdTest.Execute()
 	require.NoError(t, err)
+
+	// check if all tasks were running during same timeperiod,
+	// we can not use ps because it's not possible on Windows to get the
+	// cmdline of a running process via ps/tasklist, all parallel running
+	// tasks only show up as "bash".
+
+	type runtime struct {
+		startTime int64
+		endTime   int64
+	}
+	var taskruntimes []runtime
+	for _, logfile := range runtimeLogfiles {
+		content, err := ioutil.ReadFile(logfile)
+		require.NoError(t, err)
+		lines := strings.Fields(string(content))
+		require.Len(t, lines, 2, "%s file content: %q, expected to find 2 lines", logfile, string(content))
+		startTime, err := strconv.ParseInt(lines[0], 10, 64)
+		require.NoError(t, err)
+
+		endTime, err := strconv.ParseInt(lines[1], 10, 64)
+		require.NoError(t, err)
+		taskruntimes = append(taskruntimes, runtime{startTime: startTime, endTime: endTime})
+	}
+
+	for i := 1; i < len(taskruntimes); i++ {
+		require.GreaterOrEqual(t, taskruntimes[0].startTime, taskruntimes[i].startTime)
+		require.LessOrEqual(t, taskruntimes[0].endTime, taskruntimes[i].endTime)
+		t.Logf("task %d run in parallel with task 0: starttime %d >= %d, endtime %d <= %d",
+			i,
+			taskruntimes[i].startTime, taskruntimes[0].startTime, taskruntimes[i].endTime, taskruntimes[0].endTime,
+		)
+	}
 }
