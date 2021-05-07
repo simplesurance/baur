273a0d2a12f382b5467d1cb073157879410e4ede
Fabian Holler <fabian.holler@simplesurance.de>
2021-05-07 17:16:59 +0200

    test: run "ps -s" in TestRunSimultaneously
    "ps -s" outputs on linux and windows the processes of the current user including
    the command

--- internal/command/run_test.go
+++ internal/command/run_test.go
@@ -32,7 +32,7 @@ process_found=0
 for (( i=0; i< $parallel_tasks; i++ )); do
 	for (( j=0; ; j++ )); do
 		# [c] is needed to exclude the grep process itself from the result
-		ps -x | grep -q "[c]heckscript${i}.sh" && {
+		ps -s | grep -q "[c]heckscript${i}.sh" && {
 			echo "task $i is running"
 			break
 		}
