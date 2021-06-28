0b7f158a3be254690bef68b70587e94ac58a76e4
Fabian Holler <fabian.holler@simplesurance.de>
2021-06-28 17:13:17 +0200

    run: rename func outputsExist(9 to declaredOutputsExist()
    The previous function name sounded like it would return false if no ouputs were
    defined and no outputs existed after the task run.
    This is not the case.
    
    The new name should it make it more clear that the function returns false if the
    outputs do not exist as declared in the configuration of the task.

--- internal/command/run.go
+++ internal/command/run.go
@@ -211,7 +211,7 @@ func (c *runCmd) run(cmd *cobra.Command, args []string) {
 				return
 			}
 
-			if !outputsExist(task, outputs) {
+			if !declaredOutputsExist(task, outputs) {
 				// error is printed in runTask()
 				c.skipAllScheduledTaskRuns()
 				return
@@ -378,7 +378,7 @@ func (c *runCmd) uploadAndRecord(
 	return nil
 }
 
-func outputsExist(task *baur.Task, outputs []baur.Output) bool {
+func declaredOutputsExist(task *baur.Task, outputs []baur.Output) bool {
 	allExist := true
 
 	if len(outputs) == 0 {
