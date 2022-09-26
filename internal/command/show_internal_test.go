simplesurance/baur

commit ec95560ccf7d3ed3e0f8e60b4386e625df21ff0a

fix issues found by gocritic
Fix the following issues:

pkg/storage/postgres/query.go:304:2: ifElseChain: rewrite if-else to switch statement (gocritic)
internal/upload/filecopy/filecopy.go:78:9: elseif: can replace 'else {if cond {}}' with 'else if cond {}' (gocritic)
pkg/cfg/upgrade/v4/v4.go:134:3: assignOp: replace `p = p + "/..."` with `p += "/..."` (gocritic)
internal/testutils/dbtest/dbtest.go:55:23: wrapperFunc: use strings.ReplaceAll method in `strings.Replace(uuid.New().String(), "-", "", -1)` (gocritic)
internal/resolve/gosource/gosource_test.go:56:34: wrapperFunc: use strings.ReplaceAll method in `strings.Replace(testCfg.Cfg.Environment[i], "$WORKDIR", cwd, -1)` (gocritic)
internal/resolve/gosource/gosource_test.go:60:34: wrapperFunc: use strings.ReplaceAll method in `strings.Replace(testCfg.ExpectedResults[i], "$WORKDIR", cwd, -1)` (gocritic)
internal/testutils/repotest/repotest.go:153:18: appendAssign: append result not assigned to the same slice (gocritic)
internal/testutils/repotest/repotest.go:154:17: appendAssign: append result not assigned to the same slice (gocritic)
internal/testutils/repotest/repotest.go:116:19: badCall: suspicious Join on 1 argument (gocritic)
internal/testutils/repotest/repotest.go:117:19: badCall: suspicious Join on 1 argument (gocritic)
internal/testutils/repotest/repotest.go:196:19: badCall: suspicious Join on 1 argument (gocritic)
pkg/baur/cfgupgrade.go:65:12: wrapperFunc: use strings.ReplaceAll method in `strings.Replace(includePath, "$ROOT", repoRootDir, -1)` (gocritic)
internal/command/init_include.go:37:4: assignOp: replace `filename = filename + ".toml"` with `filename += ".toml"` (gocritic)
internal/command/show.go:93:14: appendAssign: append result not assigned to the same slice (gocritic)
internal/command/show.go:95:14: appendAssign: append result not assigned to the same slice (gocritic)

internal/command/show.go
@@ -80,19 +80,18 @@ func (c *showCmd) run(cmd *cobra.Command, args []string) {
 }
 
 func mustWriteStringSliceRows(fmt format.Formatter, header string, indentlvl int, sl []string) {
-	defRowArgs := make([]interface{}, 0, indentlvl+1+1)
+	rowArgs := make([]interface{}, 0, indentlvl+1+1)
 
 	for i := 0; i < indentlvl; i++ {
-		defRowArgs = append(defRowArgs, "")
+		rowArgs = append(rowArgs, "")
 	}
 
 	for i, val := range sl {
-		var rowArgs []interface{}
 
 		if i == 0 {
-			rowArgs = append(defRowArgs, header)
+			rowArgs = append(rowArgs, header)
 		} else {
-			rowArgs = append(defRowArgs, "")
+			rowArgs = append(rowArgs, "")
 		}
 
 		if i+1 < len(sl) {

internal/command/init_include.go
@@ -34,7 +34,7 @@ func initInclude(cmd *cobra.Command, args []string) {
 	if len(args) == 1 {
 		filename = args[0]
 		if !strings.HasSuffix(filename, ".toml") {
-			filename = filename + ".toml"
+			filename += ".toml"
 		}
 	} else {
 		filename = defIncludeFilename

internal/resolve/gosource/gosource_test.go
@@ -53,11 +53,11 @@ func TestResolve(t *testing.T) {
 			require.NoError(t, err)
 
 			for i := range testCfg.Cfg.Environment {
-				testCfg.Cfg.Environment[i] = strings.Replace(testCfg.Cfg.Environment[i], "$WORKDIR", cwd, -1)
+				testCfg.Cfg.Environment[i] = strings.ReplaceAll(testCfg.Cfg.Environment[i], "$WORKDIR", cwd)
 			}
 
 			for i := range testCfg.ExpectedResults {
-				testCfg.ExpectedResults[i] = strings.Replace(testCfg.ExpectedResults[i], "$WORKDIR", cwd, -1)
+				testCfg.ExpectedResults[i] = strings.ReplaceAll(testCfg.ExpectedResults[i], "$WORKDIR", cwd)
 				// The path separators in the test config are Unix style "/", they need to be converted to "\" when running on Windows
 				testCfg.ExpectedResults[i] = filepath.FromSlash(testCfg.ExpectedResults[i])
 			}

internal/testutils/dbtest/dbtest.go
@@ -52,5 +52,5 @@ func CreateDB(name string) (string, error) {
 
 // UniqueDBName returns a unique postgresql database name.
 func UniqueDBName() string {
-	return "baur_test" + strings.Replace(uuid.New().String(), "-", "", -1)
+	return "baur_test" + strings.ReplaceAll(uuid.New().String(), "-", "")
 }

internal/testutils/repotest/repotest.go
@@ -113,8 +113,8 @@ func (r *Repo) CreateSimpleApp(t *testing.T) *cfg.App {
 
 	r.AppCfgs = append(r.AppCfgs, &app)
 
-	buildFilePath := filepath.Join(filepath.Join(appDir, buildFile))
-	checkFilePath := filepath.Join(filepath.Join(appDir, checkFile))
+	buildFilePath := filepath.Join(appDir, buildFile)
+	checkFilePath := filepath.Join(appDir, checkFile)
 
 	fstest.WriteToFile(t, []byte(`
 #!/bin/sh
@@ -145,13 +145,16 @@ func (r *Repo) CreateAppWithNoOutputs(t *testing.T, appName string) *cfg.App {
 
 	inputFileName := fmt.Sprintf("%s.txt", appName)
 
-	shell := []string{}
-	if runtime.GOOS == "windows" {
-		shell = []string{"cmd", "/C"}
+	newCommandSlice := func() []string {
+		if runtime.GOOS == "windows" {
+			return []string{"cmd", "/C"}
+		}
+
+		return []string{}
 	}
 
-	buildCommand := append(shell, "echo", "build", appName)
-	testCommand := append(shell, "echo", "test", appName)
+	buildCommand := append(newCommandSlice(), "echo", "build", appName)
+	testCommand := append(newCommandSlice(), "echo", "test", appName)
 
 	app := cfg.App{
 		Name: appName,
@@ -193,7 +196,7 @@ func (r *Repo) CreateAppWithNoOutputs(t *testing.T, appName string) *cfg.App {
 
 	r.AppCfgs = append(r.AppCfgs, &app)
 
-	inputFilePath := filepath.Join(filepath.Join(appDir, inputFileName))
+	inputFilePath := filepath.Join(appDir, inputFileName)
 	fstest.WriteToFile(t, []byte(appName), inputFilePath)
 
 	return &app

internal/upload/filecopy/filecopy.go
@@ -75,10 +75,8 @@ func (c *Client) Upload(src string, dst string) (string, error) {
 			return "", fmt.Errorf("creating directory %s failed: %w", destDir, err)
 		}
 		c.debugLogFn("filecopy: created directory '%s'", destDir)
-	} else {
-		if !isDir {
-			return "", fmt.Errorf("%s is not a directory", destDir)
-		}
+	} else if !isDir {
+		return "", fmt.Errorf("%s is not a directory", destDir)
 	}
 
 	regFile, err := fs.IsRegularFile(dst)

pkg/baur/cfgupgrade.go
@@ -62,7 +62,7 @@ func (u *CfgUpgrader) upgradeAppConfigs(
 		log.Debugf("%s: updated", cfgPath)
 
 		for _, includePath := range appCfg.Build.Includes {
-			path := strings.Replace(includePath, "$ROOT", repoRootDir, -1)
+			path := strings.ReplaceAll(includePath, "$ROOT", repoRootDir)
 			if !filepath.IsAbs(path) {
 				path = filepath.Join(app.Path, path)
 			}

pkg/cfg/upgrade/v4/v4.go
@@ -131,7 +131,7 @@ func golangSourcesPathsToQuery(paths []string) []string {
 	result := make([]string, 0, len(paths))
 
 	for _, p := range paths {
-		p = p + "/..."
+		p += "/..."
 		result = append(result, replaceVariables(p))
 	}
 

commit 7f5f82ad1cff25f00b29e0f5266481681d25efff

show: fix: output of input slice types is broken
ec95560 broke the "baur show" output, if an input slice had >1 elements, the
field was printed multiple times, each line repeating the previous input + one
additional one.
Use again a separate slice containing the indentation elements, instead of
appending the output for each row to it.

This commit also adds a testcase for mustWriteStringSliceRows().

internal/command/show.go
@@ -79,19 +79,26 @@ func (c *showCmd) run(cmd *cobra.Command, args []string) {
 	c.showApp(arg)
 }
 
+func copyAppendSlice(slice []any, elems ...any) []any {
+	res := make([]any, len(slice))
+	copy(res, slice)
+	return append(res, elems...)
+}
+
 func mustWriteStringSliceRows(fmt format.Formatter, header string, indentlvl int, sl []string) {
-	rowArgs := make([]interface{}, 0, indentlvl+1+1)
+	defRowArgs := make([]interface{}, 0, indentlvl+1+1)
 
 	for i := 0; i < indentlvl; i++ {
-		rowArgs = append(rowArgs, "")
+		defRowArgs = append(defRowArgs, "")
 	}
 
 	for i, val := range sl {
+		var rowArgs []any
 
 		if i == 0 {
-			rowArgs = append(rowArgs, header)
+			rowArgs = copyAppendSlice(defRowArgs, header)
 		} else {
-			rowArgs = append(rowArgs, "")
+			rowArgs = copyAppendSlice(defRowArgs, "")
 		}
 
 		if i+1 < len(sl) {

internal/command/show_internal_test.go
@@ -1,0 +1,30 @@
+package command
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/simplesurance/baur/v2/internal/format/table"
+)
+
+func TestMustWriteStringSliceRows(t *testing.T) {
+	var buf bytes.Buffer
+	formatter := table.New(nil, &buf)
+
+	const hdr = "thehdr"
+	const hdrIndent = "      " // " " * len(hdr)
+	const indentlvl = 2
+	const indentStr = "        "
+	elems := []string{"one", "2", "three"}
+	mustWriteStringSliceRows(formatter, hdr, indentlvl, elems)
+	formatter.Flush()
+
+	expectedOut := ("" +
+		indentStr + indentStr + hdr + indentStr + elems[0] + ", " + "\n" +
+		hdrIndent + indentStr + indentStr + indentStr + elems[1] + ", " + "\n" +
+		hdrIndent + indentStr + indentStr + indentStr + elems[2] + "\n" +
+		"")
+	assert.Equal(t, expectedOut, buf.String())
+}