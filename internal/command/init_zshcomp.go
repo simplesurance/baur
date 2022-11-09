54ec7957138d979eb266611b85254b64c610c14f
Fabian Holler <fabian.holler@simplesurance.de>
2022-11-09 15:49:47 +0100

    add "init zshcomp" command
    Add the new command "baur init zshcomp".
    It generates a completion script and prints it to stdout.
    
    Because I could not find a way to find a user-local default directory for zsh
    completion scripts, the command does not support to install it, like "init
    bashcomp" does.

---
+++ internal/command/init_zshcomp.go
@@ -0,0 +1,38 @@
+package command
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type initZshCompCmd struct {
+	cobra.Command
+}
+
+func init() {
+	initCmd.AddCommand(&newInitZshCompCmd().Command)
+}
+
+const initZshCompLongHelp = `
+Generate a completion script for the zsh shell.
+
+The generated script is printed to stdout.
+`
+
+func newInitZshCompCmd() *initZshCompCmd {
+	cmd := initZshCompCmd{
+		Command: cobra.Command{
+			Use:   "zshcomp",
+			Short: "generate a zsh completion script",
+			Long:  initZshCompLongHelp,
+		},
+	}
+
+	cmd.Run = cmd.run
+
+	return &cmd
+}
+
+func (c *initZshCompCmd) run(_ *cobra.Command, _ []string) {
+	err := rootCmd.GenZshCompletionNoDesc(stdout)
+	exitOnErr(err)
+}
