fc4d01ad865ccf640979b1faad70c09d34c4094e
Fabian Holler <fabian.holler@simplesurance.de>
2024-01-23 17:05:11 +0100

    gitobjectid: rename Calc to Hash
    same name as in sha384 package

--- internal/digest/gitobjectid/gitobjectid.go
+++ internal/digest/gitobjectid/gitobjectid.go
@@ -9,22 +9,22 @@ import (
 
 type PrintfFn func(format string, a ...any)
 
-// Calc calculates git object IDs for files.
+// Hash calculates git object IDs for files.
 // Object IDs are read the first time a digest is requested from the git repository.
-type Calc struct {
+type Hash struct {
 	repoDir   string
 	debugLogf PrintfFn
 }
 
-func New(gitRepositoryDir string, debugLogf PrintfFn) *Calc {
-	return &Calc{
+func New(gitRepositoryDir string, debugLogf PrintfFn) *Hash {
+	return &Hash{
 		repoDir:   gitRepositoryDir,
 		debugLogf: debugLogf,
 	}
 }
 
 // File calculcates the git object ID for the file at absPath.
-func (h *Calc) File(absPath string) (*digest.Digest, error) {
+func (h *Hash) File(absPath string) (*digest.Digest, error) {
 	h.debugLogf("gitobjectid: object id not in cache, calculating ID for %q", absPath)
 	objectID, err := git.ObjectID(context.TODO(), absPath, h.repoDir)
 	if err != nil {
