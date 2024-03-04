d68378dd7b5abcbbb7def09c5e636c8fea789443
Fabian Holler <fabian.holler@simplesurance.de>
2024-03-04 15:52:36 +0100

    resolve/gosource: add rel file query with multiple main packages test
    Add a testcase for the gosource resolver that specifies as file query pattern
    cmd/**/main.go and runs the goresolver from a different directory then the test
    (aka app dir) directory.
    
    This testcase currently fails because the relative file query patterns are not
    relative to the specified working dir (app dir) but to the cwd
    (https://github.com/golang/go/issues/65965).
    
    The existing testcase query_main_file is refactored for this purpose
    The test_config.json files got a new field WorkingDir, the $WORKDIR
    test_config.json variable is renamed to $TESTDIR.

---
+++ internal/resolve/gosource/testdata/multiple_mains_doublestar_file_query/cmd/two/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/simplesurance/baur-test/intstr"
+)
+
+func main() {
+	fmt.Println(intstr.One)
+}
---
+++ internal/resolve/gosource/testdata/multiple_mains_doublestar_file_query/intstr/intstr.go
@@ -0,0 +1,3 @@
+package intstr
+
+const One = "One"
